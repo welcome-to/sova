d5eee8ddf9ceb929db897586d156896afb59c3f0
arhip <arhip.arhip2002@yandex.ru>
2019-12-09 22:37:01 +0300

    added something

--- sova.py
+++ sova.py
@@ -10,13 +10,58 @@ class Operator:
         assert len(args) == arity, "Wrong number of arguments"
         return self.func(args)
 
-class Composition
+class Composition(Object):
+    def __init__(self,operator, args_list):
+        self.operator = operator
+        self.args = args_list
+
+
+
+
 class And(Operator):
     def __init__(self):
         And.super().__init__(lambda x,y: x and y, 2)
 
-    def __str__(self):
-        return "(" + 
+
+
+class Not(Operator):
+    def __init__(self):
+        Not.super().__init__(lambda x: not x, 1)
+
+
+class Or(Operator):
+    def __init__(self):
+        Or.super().__init__(lambda x,y: x or y, 2)
+
+
+class Xor(Operator):
+    def __init__(self):
+        Xor,super().__init__(lambda x,y:not x == y, 2)
+
+
+class Eq(Operator):
+    def __init__(self):
+        Eq.super().__init__(lambda x,y: x == y, 2)
+
+
+class Impl(Object):
+    def __init__(self):
+        Impl.super().__init__(lambda x, y: if y == 0 and x == 1: False else True, 2)
+
+
+
+class Nand(Operator):
+    def __init__(self):
+        Nand.super().__init__(lambda x , y: not x and y, 2)
+
+
+
+class Nor(Operator):
+    def __init__(self):
+        Nor.super().__init__(lambda x ,y: not x or y, 2)
+
+
+
 
 priorities = {
     'not': 0,
@@ -29,7 +74,20 @@ priorities = {
     'equiv': 5
 }
 
-def parse(line):
+FuncType = {
+    'not'  : Not,
+    'and'  : And,
+    'nand' : Nand,
+    'or'   : Or,
+    'nor'  : Nor,
+    'xor'  : Xor,
+    'impl' : Impl,
+    'equiv': Eq
+
+}
+
+
+def parser_copy(line):
     is_in_brackets = True
     balance = 0
     for (index, char) in enumerate(line):
@@ -47,21 +105,27 @@ def parse(line):
         return parse(line[1:n-1])
 
     prior_operator, prior_start = None, None
+    balance = 0
     for start in range(len(name)):
-        for name in priorities.keys():
-            if current.startswith(name):
-                if not prior_operator or priorities[name] >= priorities[prior_operator]:
-                    prior_operator, prior_start = name, start
-    
+        if line[start] == '(':
+            balance += 1
+        if line[start] == ')':
+            balance -= 1
+        if balance == 0:
+            for name in priorities.keys():
+                if line.startswith(name):
+                    if not prior_operator or priorities[name] >= priorities[prior_operator]:
+                        prior_operator, prior_start = name, start
+
     if prior_operator is not None:
-        if prior_start == 0: # unary
-            return Composition(Not(), [parse(line[3:])])
+        if prior_start == 0:
+            return Composition(Not(), [parser_copy(line[3:])])
         return Composition(
-            FuncType(prior_operator),
-            [parse(line[:prior_start]), parse(line[prior_start + len(prior_operator):])]
-        ) # FIXME
+            FuncType[prior_operator](),
+            [parser_copy(line[:prior_start]),parse(line[prior_start + len(prior_operator):])]
+            )
 
-    if line in ['1', '0']:
+    if line in ['1','0']:
         return Constant(bool(int(line)))
-
+    return Variable(line)
     
